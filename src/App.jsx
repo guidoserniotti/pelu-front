f18679fbb80b818b815d63f9a98e970ab8e4dd22
guidoserniotti <guido.serniotti.13@gmail.com>
2025-10-20 11:30:05 -0300

    login, local storage & (edit client not working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,130 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import authService from "../utils/config";
 import Clients from "./components/Clients";
 import Calendar from "./components/FullCalendar";
-import loginService from "./services/login";
 import LoginForm from "./components/LoginForm";
-
+import loginService from "./services/login";
 import "./styles/App.css";
 
+/* TO-DO:
+- Persistencia de sesión (localStorage)
+- LOGOUT:
+  * Limpiar token
+  * Set isLogin to false
+- Manejo de clientes:
+  * Editar cliente (FALTA CONEXIÓN CON BACK)
+  * Eliminar cliente (FALTA CONEXIÓN CON BACK)
+- Conexión con backend:
+  * Autenticación
+  * CRUD clientes
+    ~ Formatear clientData para utilizar "nombre_completo" del backend
+- Manejo de eventos:
+  * CRUD eventos
+  * Crear evento al arrastrar cliente al calendario
+- Estilo y UX:
+  * Mejorar diseño
+  * Feedback al usuario (notificaciones, loaders, etc.)
+  * Responsividad
+  * IMPLEMENTAR PAGINACIÓN
+    ~ Estado para página actual
+    ~ Petición de backend para calculo de páginas.
+    ~ La última página pregunta a la siguiente si tiene datos.
+    ~ Scroll de máximos clientes
+- Validaciones:
+  * Manejo de errores (login fallido, errores de red, etc.)
+*/
+
 const App = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
-  const [isLogin, setIsLogin] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLogin, setIsLogin] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
 
-  const [client, setClient] = useState([
-    {
-      title: "Cliente 1",
-      start: "2023-10-05T10:00:00",
-      end: "2023-10-05T12:00:00",
-      editable: true,
-    },
-    {
-      title: "Cliente 2",
-      start: "2023-10-06T10:00:00",
-      end: "2023-10-06T12:00:00",
-      editable: true,
-    },
-  ]);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const user = await loginService.login({
+                email,
+                contrasena: password,
+            });
+            // Guardar con estructura consistente
+            window.localStorage.setItem(
+                "loggedUser",
+                JSON.stringify({
+                    email: email,
+                    token: user.data, // user.data es el token
+                })
+            );
+            authService.setToken(user.data);
+            setIsLogin(true);
+            setEmail("");
+            setPassword("");
+        } catch (exception) {
+            console.log(exception);
+            setErrorMessage(exception.response.data.message);
+            setTimeout(() => {
+                setErrorMessage("");
+            }, 5000);
+        }
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const user = await loginService.login({
-        email,
-        contrasena: password,
-      });
-      window.localStorage.setItem("loggedUser", JSON.stringify(user));
-      setUser(user);
-      setIsLogin(true);
-      console.log(user);
-      console.log("Login exitoso");
-      setEmail("");
-      setPassword("");
-    } catch (exception) {
-      console.log(exception);
-      setErrorMessage(exception.response.data.message);
-      setTimeout(() => {
-        setErrorMessage("");
-      }, 5000);
+    const handleInvalidInput = (e) => {
+        if (e.target.validity.valueMissing) {
+            e.target.setCustomValidity("Este campo es obligatorio");
+        } else if (e.target.validity.typeMismatch) {
+            e.target.setCustomValidity("Por favor, ingrese un email válido");
+        }
+    };
+
+    const handleLogOut = () => {
+        const confirmLogOut = window.confirm(
+            `¿Estás seguro de que deseas cerrar sesión?`
+        );
+        if (!confirmLogOut) return;
+        window.localStorage.removeItem("loggedUser");
+        window.location.reload();
+    };
+
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem("loggedUser");
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON);
+            authService.setToken(user.token);
+            setIsLogin(true);
+        }
+    }, []);
+
+    if (!isLogin) {
+        return (
+            <>
+                <div className="main-calendar-container">
+                    <LoginForm
+                        handleInvalidInput={handleInvalidInput}
+                        handleSubmit={handleSubmit}
+                        errorMessage={errorMessage}
+                        email={email}
+                        password={password}
+                        setEmail={setEmail}
+                        setPassword={setPassword}
+                        mailError={emailError}
+                        passwordError={passwordError}
+                        setEmailError={setEmailError}
+                        setPasswordError={setPasswordError}
+                    />
+                </div>
+            </>
+        );
     }
-  };
-
-  const handleInvalidInput = (e) => {
-    if (e.target.validity.valueMissing) {
-      e.target.setCustomValidity("Este campo es obligatorio");
-    } else if (e.target.validity.typeMismatch) {
-      e.target.setCustomValidity("Por favor, ingrese un email válido");
-    }
-  };
-
-  if (!isLogin) {
     return (
-      <>
-        <div className="main-calendar-container">
-          <LoginForm
-            handleInvalidInput={handleInvalidInput}
-            handleSubmit={handleSubmit}
-            errorMessage={errorMessage}
-            email={email}
-            password={password}
-            setEmail={setEmail}
-            setPassword={setPassword}
-            mailError={emailError}
-            passwordError={passwordError}
-            setEmailError={setEmailError}
-            setPasswordError={setPasswordError}
-          />
-        </div>
-      </>
+        <>
+            <div className="main-calendar-container">
+                <Clients handleLogOut={handleLogOut} />
+                <Calendar />
+            </div>
+        </>
     );
-  }
-  return (
-    <>
-      <div className="main-calendar-container">
-        <Clients client={client} setClient={setClient} />
-        <Calendar />
-      </div>
-    </>
-  );
 };
 
 export default App;
